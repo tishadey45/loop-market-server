4ae3cd7e07d57ffae4566aea8c46d89f819a1080
Tisha Dey <tishadey2025@outlook.com>
2026-06-28 22:02:27 +0600

    initial commit

---
+++ index.js
@@ -0,0 +1,64 @@
+require("dotenv").config();
+const express = require("express");
+const cors = require("cors");
+
+
+const app = express();
+const port = process.env.PORT || 5000;
+
+//  middleware
+app.use(
+  cors({
+    origin: true,
+  })
+);
+app.use(express.json());
+
+
+const { MongoClient, ServerApiVersion, ObjectId } = require("mongodb");
+const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASSWORD}@cluster0.d2ts7wd.mongodb.net/?appName=Cluster0`;
+
+const client = new MongoClient(uri, {
+  serverApi: {
+    version: ServerApiVersion.v1,
+    strict: true,
+    deprecationErrors: true,
+  },
+});
+
+
+
+async function run() {
+  try {
+    await client.db("admin").command({ ping: 1 });
+    console.log(
+      "Pinged your deployment. You successfully connected to MongoDB ✅",
+    );
+    const db = client.db("study-nook");
+    const productsCollection = db.collection("products");
+
+    app.get("/products", async (req, res) => {
+      const result = await productsCollection.find().toArray();
+      res.send(result);
+    });
+
+    app.get("/products/:id", async (req, res) => {
+      const id = req.params.id;
+      console.log(id);
+      const query = { _id: new ObjectId(id) };
+      const result = await productsCollection.findOne(query);
+      res.send(result);
+    });
+
+  } finally {
+  }
+}
+run().catch(console.dir);
+
+app.get("/", (req, res) => {
+  res.send("loop market server is running successfully ✅");
+});
+
+app.listen(port, () => {
+  console.log(`Example app listening on port ${port}`);
+});
